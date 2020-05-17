412cfb23a50b573c035e8b245fa83bbcba6efeb2
mostfunkyduck <mostfunkyduck@protonmail.com>
2020-05-17 06:47:38 -0400

    adding some packages to bootstrap, adding tags file to global .gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PACKAGES="jq vim tmux ctags sysstat shellcheck"
+PACKAGES="jq vim tmux ctags sysstat shellcheck golang pipenv"
 if command -v X; then
   PACKAGES+=('xclip')
 fi
@@ -14,10 +14,12 @@ if ! command -v brew; then
 fi
 echo installing packages, sudo required
 if command -v apt-get; then
-  sudo apt-get -y install "${PACKAGES[@]}" build-essential
+  # shellcheck disable=SC2128,SC2086
+  sudo apt-get -y install $PACKAGES build-essential
 elif command -v yum; then
   sudo yum -y groupinstall "Development Tools"
-  sudo yum install -y "${PACKAGES[@]}"
+  # shellcheck disable=SC2128,SC2086
+  sudo yum install -y $PACKAGES
 else
   echo "could not find a package manager!"
 fi
@@ -47,14 +49,14 @@ echo -ne "\tpathogen: "
 mkdir -p "$HOME/.vim/swpfiles"
 mkdir -p "$HOME/.vim/autoload" "$HOME/.vim/bundle" && \
 if [[ ! -f $HOME/.vim/autoload/pathogen.vim ]]; then
-  curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim 2>/dev/null && echo "ok" || echo "didn't install pathogen"
+  curl -LSso "$HOME/.vim/autoload/pathogen.vim" https://tpo.pe/pathogen.vim 2>/dev/null | tr -d "\n" && echo "ok" || echo "didn't install pathogen"
 fi
 
 function pull_or_clone() {
   if [ -d "$2" ]; then
-    git -C "$2" pull && echo "ok" || echo "couldn't pull $1"
+    git -C "$2" pull || echo "couldn't pull $1"
   else
-    git clone "$1" "$2" && echo "ok" || echo "couldn't clone $1"
+    git clone "$1" "$2" || echo "couldn't clone $1"
   fi  
 }
 
