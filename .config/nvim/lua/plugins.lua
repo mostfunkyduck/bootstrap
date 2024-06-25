609d702e0b49ee1d9022079bd3489099413100f6
Jack Kuperman <173180712+jkuperman-prizepicks@users.noreply.github.com>
2024-06-25 11:44:56 -0400

    stuff

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,6 +2,75 @@ vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
 	use({
+		-- I think this was supposed to be a dependency? FIXME!
+		{
+			"williamboman/mason.nvim",
+		},
+		-- the lsp magic
+		{
+			"neovim/nvim-lspconfig",
+			config = function()
+				local lspconfig = require("lspconfig")
+				lspconfig.tsserver.setup({})
+				lspconfig.gopls.setup({})
+				lspconfig.jsonls.setup({})
+				lspconfig.csharp_ls.setup({})
+				lspconfig.terraformls.setup({
+					filetypes = { "terraform", "tf", "terraform-vars" },
+				})
+				lspconfig.pyright.setup({})
+				lspconfig.bashls.setup({})
+				lspconfig.gdscript.setup({})
+				lspconfig.lua_ls.setup({})
+				lspconfig.rust_analyzer.setup({
+					settings = {
+						["rust-analyzer"] = {},
+					},
+				})
+				lspconfig.gdscript.setup({})
+
+				-- Global mappings.
+				-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+				vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+				vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+				vim.keymap.set("n", "<space>qf", vim.diagnostic.setqflist)
+
+				-- Use LspAttach autocommand to only map the following keys
+				-- after the language server attaches to the current buffer
+				vim.api.nvim_create_autocmd("LspAttach", {
+					group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+					callback = function(ev)
+						-- Enable completion triggered by <c-x><c-o>
+						vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+						-- Buffer local mappings.
+						-- See `:help vim.lsp.*` for documentation on any of the below functions
+						local opts = { buffer = ev.buf }
+						vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+						vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+						vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+						vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+						vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+						vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+						vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+						vim.keymap.set("n", "<space>wl", function()
+							print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+						end, opts)
+						vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+						vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+						vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+						vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+						vim.keymap.set("n", "<space>f", function()
+							vim.lsp.buf.format({ async = true })
+						end, opts)
+					end,
+				})
+			end,
+		},
+	})
+	use({
 		"nvim-lualine/lualine.nvim",
 
 		requires = {
@@ -501,102 +570,6 @@ set nofoldenable
 		"folke/neoconf.nvim",
 	})
 	require("neoconf").setup()
-	use({
-		-- I think this was supposed to be a dependency? FIXME!
-		{
-			"williamboman/mason.nvim",
-		},
-		-- the lsp magic
-		{
-			"neovim/nvim-lspconfig",
-			config = function()
-				local lspconfig = require("lspconfig")
-				local capabilities = require("cmp_nvim_lsp").default_capabilities()
-				lspconfig.tsserver.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.gopls.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.jsonls.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.csharp_ls.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.terraformls.setup({
-					capabilities = capabilities,
-					filetypes = { "terraform", "tf", "terraform-vars" },
-				})
-				lspconfig.pyright.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.bashls.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.gdscript.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.lua_ls.setup({
-					capabilities = capabilities,
-				})
-				lspconfig.groovyls.setup({
-					capabilities = capabilities,
-					cmd = {
-						"/opt/homebrew/opt/openjdk@17/bin/java",
-						"-jar",
-						"/Users/Jack.Kuperman/code/groovy-language-server/build/libs/groovy-language-server-all.jar",
-					},
-				})
-				lspconfig.rust_analyzer.setup({
-					capabilities = capabilities,
-					settings = {
-						["rust-analyzer"] = {},
-					},
-				})
-				lspconfig.gdscript.setup({})
-
-				-- Global mappings.
-				-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-				vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-				vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-				vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
-				vim.keymap.set("n", "<space>qf", vim.diagnostic.setqflist)
-
-				-- Use LspAttach autocommand to only map the following keys
-				-- after the language server attaches to the current buffer
-				vim.api.nvim_create_autocmd("LspAttach", {
-					group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-					callback = function(ev)
-						-- Enable completion triggered by <c-x><c-o>
-						vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-						-- Buffer local mappings.
-						-- See `:help vim.lsp.*` for documentation on any of the below functions
-						local opts = { buffer = ev.buf }
-						vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-						vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-						vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-						vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-						vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-						vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-						vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-						vim.keymap.set("n", "<space>wl", function()
-							print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-						end, opts)
-						vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-						vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-						vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-						vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-						vim.keymap.set("n", "<space>f", function()
-							vim.lsp.buf.format({ async = true })
-						end, opts)
-					end,
-				})
-			end,
-		},
-	})
 	-- Moar fzf
 	use("junegunn/fzf")
 	-- MOAR fzf
